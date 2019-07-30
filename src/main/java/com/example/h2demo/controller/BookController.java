31071c349b9f0b204ecd29bbb94b5ccf3ef781bb
Dex Deb <dexdeb246@gmail.com>
2019-07-30 15:08:41 +0530

    Initial Add

---
+++ src/main/java/com/example/h2demo/controller/BookController.java
@@ -0,0 +1,48 @@
+package com.example.h2demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.h2demo.domain.Book;
+import com.example.h2demo.service.BookService;
+
+@RestController
+public class BookController {
+
+	@Autowired
+	BookService bookservice;
+	
+
+	@GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return bookservice.getAllBooks();
+    }
+
+    @GetMapping("/books/{id}")
+    public Book getBook(@PathVariable("id") String id){
+        return bookservice.getBook(id);
+    }
+
+    @PostMapping("/books")
+    public void addBook(@RequestBody Book book) {
+        bookservice.addBook(book);
+    }
+
+    @PutMapping("/books/{id}")
+    public void updateBook(@PathVariable("id") String bookId, @RequestBody Book book) {
+        bookservice.updateBook(bookId, book);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public void deleteBook(@PathVariable("id") String bookId) {
+        bookservice.deleteBook(bookId);
+    }
+}
---
+++ src/main/java/com/example/h2demo/service/BookService.java
@@ -0,0 +1,40 @@
+package com.example.h2demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.h2demo.domain.Book;
+import com.example.h2demo.repository.BookRepository;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        List<Book> books = new ArrayList<>();
+        bookRepository.findAll().forEach(books::add);
+        return books;
+    }
+
+    public Book getBook(String id) {
+        return bookRepository.findById(id).orElseGet(Book::new);
+    }
+
+    public void addBook(Book whiskey) {
+        bookRepository.save(whiskey);
+    }
+
+    public void updateBook(String id, Book whiskey) {
+        bookRepository.save(whiskey);
+    }
+
+    public void deleteBook(String id) {
+        bookRepository.deleteById(id);
+    }
+
+}
